cab2619515cb9de468f7c51feac43b8dc563b40c
READTalk <redtalk.app@hotmail.com>
2026-06-05 23:21:18 +0700

    Delete app/routes/chat.$id.tsx

--- app/routes/chat.$id.tsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from "react-router";
-import ChatRoom from "~/components/ChatRoom";
-
-export default function ChatRoute() {
-  const { id } = useParams();
-  return <ChatRoom chatId={id} />;
-}
